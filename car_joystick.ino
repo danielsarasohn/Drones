29d00ea21e31c06ecc81013ca8470d33f9df2175
danielsarasohn <59397382+danielsarasohn@users.noreply.github.com>
2021-02-03 14:47:50 -0500

    Update car_joystick.ino

--- car_joystick.ino
+++ car_joystick.ino
@@ -20,45 +20,37 @@ void setup() {
 }
 
 void loop() {
-  int xAxis = analogRead(A0); // Read Joysticks X-axis
-  int yAxis = analogRead(A1); // Read Joysticks Y-axis
+  int xAxis = analogRead(A0);
+  int yAxis = analogRead(A1); 
 
-  // Y-axis used for forward and backward control
   if (yAxis < 470) {
-    // Set Motor A backward
+
     digitalWrite(in1, HIGH);
     digitalWrite(in2, LOW);
-    // Set Motor B backward
+   
     digitalWrite(in3, HIGH);
     digitalWrite(in4, LOW);
-    // Convert the declining Y-axis readings for going backward from 470 to 0 into 0 to 255 value for the PWM signal for increasing the motor speed
+   
     motorSpeedA = map(yAxis, 470, 0, 0, 255);
     motorSpeedB = map(yAxis, 470, 0, 0, 255);
   }
   else if (yAxis > 550) {
-    // Set Motor A forward
     digitalWrite (in1, LOW);
     digitalWrite (in2, HIGH);
     digitalWrite (in3, LOW);
     digitalWrite (in4, HIGH);
-    // Convert the increasing Y-axis readings for going forward from 550 to 1023 into 0 to 255 value for the PWM signal for increasing the motor speed
     motorSpeedA = map(yAxis, 550, 1023, 0, 255);
     motorSpeedB = map(yAxis, 550, 1023, 0, 255);
   }
-  // If joystick stays in middle the motors are not moving
   else {
     motorSpeedA = 0;
     motorSpeedB = 0;
   }
 
-  // X-axis used for left and right control
   if (xAxis < 470) {
-    // Convert the declining X-axis readings from 470 to 0 into increasing 0 to 255 value
     int xMapped = map(xAxis, 470, 0, 0, 255);
-    // Move to left - decrease left motor speed, increase right motor speed
     motorSpeedA = motorSpeedA - xMapped;
     motorSpeedB = motorSpeedB + xMapped;
-    // Confine the range from 0 to 255
     if (motorSpeedA < 0) {
       motorSpeedA = 0;
     }
@@ -67,12 +59,9 @@ void loop() {
     }
   }
   if (xAxis > 550) {
-    // Convert the increasing X-axis readings from 550 to 1023 into 0 to 255 value
     int xMapped = map(xAxis, 550, 1023, 0, 255);
-    // Move right - decrease right motor speed, increase left motor speed
     motorSpeedA = motorSpeedA + xMapped;
     motorSpeedB = motorSpeedB - xMapped;
-    // Confine the range from 0 to 255
     if (motorSpeedA > 255) {
       motorSpeedA = 255;
     }
@@ -85,6 +74,6 @@ void loop() {
   if (motorSpeedB < 70) {
     motorSpeedB = 0;
   }
-  analogWrite(enA, motorSpeedA); // Send PWM signal to motor A
-  analogWrite(enB, motorSpeedB); // Send PWM signal to motor B
+  analogWrite(enA, motorSpeedA);
+  analogWrite(enB, motorSpeedB);
 }
